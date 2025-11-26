e7c5872373bdd445fc62861bf63e719d479f3a06
si-moon-is <153811569+si-moon-is@users.noreply.github.com>
2025-11-26 09:44:51 +0100

    Update advent-calendar.php

--- advent-calendar.php
+++ advent-calendar.php
@@ -3,7 +3,7 @@
  * Plugin Name: Super Kalendarz Adwentowy
  * Plugin URI: https://twoja-strona.pl
  * Description: Dodaj piękny, interaktywny kalendarz adwentowy na swojej stronie WordPress
- * Version: 2.0.2
+ * Version: 2.0.3
  * Author: Szymon Koscikiewicz
  * License: GPL v2 or later
  * Text Domain: advent-calendar
@@ -15,18 +15,15 @@ if (!defined('ABSPATH')) {
 }
 
 // Definicje stałych
-define('ADVENT_CALENDAR_VERSION', '2.0.2');
+define('ADVENT_CALENDAR_VERSION', '2.0.3');
 define('ADVENT_CALENDAR_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('ADVENT_CALENDAR_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
-// Najpierw ładujemy zależności
+// Ładujemy zależności
 require_once ADVENT_CALENDAR_PLUGIN_PATH . 'includes/class-advent-calendar.php';
 require_once ADVENT_CALENDAR_PLUGIN_PATH . 'includes/class-advent-calendar-admin.php';
 require_once ADVENT_CALENDAR_PLUGIN_PATH . 'includes/class-advent-calendar-frontend.php';
 require_once ADVENT_CALENDAR_PLUGIN_PATH . 'includes/class-advent-calendar-ajax.php';
-require_once ADVENT_CALENDAR_PLUGIN_PATH . 'includes/class-advent-calendar-export-import.php';
-require_once ADVENT_CALENDAR_PLUGIN_PATH . 'includes/class-advent-calendar-statistics.php';
-require_once ADVENT_CALENDAR_PLUGIN_PATH . 'includes/class-advent-calendar-styles.php';
 
 // Główna klasa wtyczki
 class Advent_Calendar_Plugin {
@@ -49,27 +46,21 @@ class Advent_Calendar_Plugin {
     public function init() {
         load_plugin_textdomain('advent-calendar', false, dirname(plugin_basename(__FILE__)) . '/languages');
         
-        // Rejestracja hooków
+        // Inicjalizacja komponentów
         if (is_admin()) {
             new Advent_Calendar_Admin();
-        } else {
-            new Advent_Calendar_Frontend();
         }
         
+        new Advent_Calendar_Frontend();
         new Advent_Calendar_Ajax();
-        new Advent_Calendar_Export_Import();
-        new Advent_Calendar_Statistics();
-        new Advent_Calendar_Styles();
         
-        // Rejestracja shortcode
+        // Shortcode
         add_shortcode('advent_calendar', array($this, 'calendar_shortcode'));
     }
     
     public function activate() {
-        // Tworzenie tabel w bazie danych
         Advent_Calendar::create_tables();
         
-        // Domyślne ustawienia
         add_option('advent_calendar_default_settings', array(
             'columns' => 6,
             'rows' => 4,
@@ -94,6 +85,6 @@ class Advent_Calendar_Plugin {
     }
 }
 
-// Inicjalizacja wtyczki
+// Inicjalizacja
 Advent_Calendar_Plugin::get_instance();
 ?>

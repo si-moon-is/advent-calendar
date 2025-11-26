a4e088a268ab37cc4059b94963a4d8baa9c5168f
si-moon-is <153811569+si-moon-is@users.noreply.github.com>
2025-11-26 10:03:19 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,10 @@
 <?php
-// Zabezpieczenie przed bezpośrednim dostępem
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
 global $wpdb;
 
-// Usuwanie tabel
 $tables = array(
     'advent_calendars',
     'advent_calendar_doors',
@@ -18,10 +16,8 @@ foreach ($tables as $table) {
     $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}{$table}");
 }
 
-// Usuwanie opcji
 delete_option('advent_calendar_default_settings');
 delete_option('advent_calendar_version');
 delete_option('advent_calendar_db_version');
 
-// Usuwanie transjentów
 $wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE '%advent_calendar%'");
